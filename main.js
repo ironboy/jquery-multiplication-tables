f3ceb1cdb06c82d8ac0ca5d83682f05991f9c409
Ironboy <ironboy@users.noreply.github.com>
2024-12-04 12:31:24 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 // $ = jQuery
-// 'jQuery please grab the body element and add a heading saying 'Hej'
-$('body').append('<h1>Hej!</h1>');
+// 'jQuery please grab the body element and add a heading
+$('body').append('<h1>Multiplication tables</h1>');
 
 // Write a multiplication table to the DOM using jQuery
-let table = 12;
+function createMultiplicationTable(table) {
+  // add a heading
+  $('body').append('<h2>Table ' + table + '</h2>');
+  // add paragraphs with each line in the table
+  for (let i = 1; i <= 12; i++) {
+    $('body').append(
+      '<p>' + i + ' x ' + table + ' = ' + (i * table) + '</p>'
+      //  Shorter strings are possible with template literals:
+      // `<p>${i} x ${table} = ${i * table}</p>`
+    );
+  }
+}
+
+// Call the createMultiplicationTable 12 times in a loop
 for (let i = 1; i <= 12; i++) {
-  $('body').append(
-    '<p>' + i + ' x ' + table + ' = ' + (i * table) + '</p>'
-    //  Shorter strings are possible with template literals:
-    // `<p>${i} x ${table} = ${i * table}</p>`
-  );
+  createMultiplicationTable(i);
 }
